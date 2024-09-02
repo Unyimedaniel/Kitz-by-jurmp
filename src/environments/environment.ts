f83e1710d9bad1844c1086cd8ff04532fd3cdff3
Jurmp <unyimedaniel@gmail.com>
2024-09-02 17:41:27 +0100

    env

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    projectId: 'kitz-app-gcp',
+    appId: '1:305864784774:web:d95d5c6b2c64600f180a07',
+    storageBucket: 'kitz-app-gcp.appspot.com',
+    apiKey: 'AIzaSyB07WqKGrubqVLTSjMhMViJeVIq_uMIJLI',
+    authDomain: 'kitz-app-gcp.firebaseapp.com',
+    messagingSenderId: '305864784774',
+    measurementId: 'G-41Y4PMFFWH',
+  },
+  graphql: {
+    uri: 'http://localhost:3000/graphql'
+  }
+};
